c359e9117fa95c594f856103ad555b6bcda9a684
pnl0j327 <zandhuiseva@gmail.com>
2024-04-02 16:48:34 +0200

    in playground, automate DateFile (instead of manually hardcode it)

--- RAWSimO.Core/ReadData.cs
+++ RAWSimO.Core/ReadData.cs
@@ -2,24 +2,26 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RAWSimO.Core
 {
+    
     public class ReadData
     {
         public static List<PodItem> PodContentData { get; }
         public static int TotalSkuCount { get; private set; }
-        public static Dictionary<int, int> CapacityUsedPerPod { get; }
+        public static Dictionary<int, int> CapacityUsedPerPod { get; } 
 
         static ReadData()
         {
             PodContentData = new List<PodItem>();
             TotalSkuCount = 0;
             CapacityUsedPerPod = new Dictionary<int, int>();
-
-            string csvFilePath = "C:\\Users\\pnl0j327\\PycharmProjects\\pythonProject1\\large base scenario.csv";
+            
+            string csvFilePath = GetCsvFilePath();
 
             using (StreamReader reader = new StreamReader(csvFilePath))
             {
@@ -45,12 +47,47 @@ namespace RAWSimO.Core
                                 CapacityUsedPerPod[podIndex] += skuCount;
                             else
                                 CapacityUsedPerPod[podIndex] = skuCount;
-
-                        }  
+                        }
                     }
                 }
             }
         }
+
+        private static string GetCsvFilePath()
+        {
+            // comment or uncomment line below 
+            // for visualization: uncomment                       for playground: comment
+            //return "C:\\Users\\pnl0j327\\PycharmProjects\\pythonProject1\\large base scenario.csv";
+            
+            
+            string folderPath = @"C:\Users\pnl0j327\PycharmProjects\pythonProject1\";
+            string[] csvFiles = Directory.GetFiles(folderPath, "*.csv");
+
+            if (csvFiles.Length == 0)
+            {
+                Console.WriteLine("No CSV files found in the specified folder.");
+                return null;
+            }
+
+            Console.WriteLine("Choose a CSV file:");
+
+            for (int i = 0; i < csvFiles.Length; i++)
+            {
+                Console.WriteLine($"{i}. {Path.GetFileName(csvFiles[i])}");
+            }
+
+
+            if (int.TryParse(Console.ReadLine(), out int selectedIndex) && selectedIndex >= 0 && selectedIndex < csvFiles.Length)
+            {
+                return csvFiles[selectedIndex];
+            }
+            else
+            {
+                Console.WriteLine("Invalid selection. Please enter a valid index.");
+                return null;
+            }
+        }
+
     }
 
     public class PodItem
